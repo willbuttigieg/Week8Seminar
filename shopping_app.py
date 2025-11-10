7bed575f17519b2588721c51d63f4843019287e1
William Buttigieg <williambuttigieg2005@gmail.com>
2025-11-11 09:55:18 +1000

    Add collaborators markdown file for name keeping

--- shopping_app.py
+++ shopping_app.py
@@ -1,15 +1,26 @@
 """
 CP1404 Seminar 8
 """
+from kivy.app import App
+from kivy.core.window import Window
+from kivy.lang import Builder
+from kivy.uix.button import Button
+from kivy.properties import StringProperty
 
-class ShoppingApp:
-    """Product app class."""
+class ShoppingApp(App):
+    """Shopping app class."""
 
-    def __init__(self):
+    def __init__(self, **kwargs):
         """Construct main kivy app."""
+        super().__init__(**kwargs)
+        Window.size = (800, 600)
+        self.title = "Shopping App"
 
     def build(self):
         """Build main kivy app."""
+        Window.size = (800, 600)
+        self.title = "Shopping App"
+
 
 
     def create_widgets(self):
